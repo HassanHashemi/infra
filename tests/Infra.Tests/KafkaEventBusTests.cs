bf8d3673372ae81f2529e0239d3e4300d21592c2
meysambagheri96 <Meysambagheri1994@gmail.com>
2024-08-09 17:02:50 +0330

    add storage

--- tests/Infra.Tests/KafkaEventBusTests.cs
+++ tests/Infra.Tests/KafkaEventBusTests.cs
@@ -14,6 +14,7 @@ public class KafkaEventBusTests
 		var provider = new ContainerBuilder()
             .AddLoggingInternal()
 			.AddKafkaEventBusInternal()
+            .AddTestResultStorageInternal()
 			.Build();
 
 		var bus = provider.Resolve<IEventBus>();
